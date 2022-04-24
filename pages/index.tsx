0f0f14fc0af0399ed14b430c6228d4f36fcc2682
Daniel Unapanta <dunapanta@outlook.es>
2022-04-23 21:37:42 -0500

    Add Card for display pokemons

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,6 +4,7 @@ import { Layout } from "components/layouts";
 import pokemonApi from "api/pokemonApi";
 import { PokemonData, PokemonListResponse } from "interfaces/pokemonInterfaces";
 import Image from "next/image";
+import { Card, Grid, Row, Text } from "@nextui-org/react";
 
 interface Props {
   pokemons: PokemonData[];
@@ -12,17 +13,23 @@ interface Props {
 const HomePage: NextPage<Props> = ({ pokemons }) => {
   return (
     <Layout title="Pokémon primera generación">
-      {pokemons.map((pokemon) => (
-        <ul key={pokemon.id}>
-          <h1>{pokemon.name}</h1>
-          <Image
-            src={pokemon.img}
-            alt={pokemon.name}
-            width={100}
-            height={100}
-          />
-        </ul>
-      ))}
+      <Grid.Container gap={2} justify="flex-start">
+        {pokemons.map((pokemon) => (
+          <Grid key={pokemon.id} xs={6} sm={3} md={2} xl={1}>
+            <Card hoverable clickable>
+              <Card.Body css={{ p: 1 }}>
+                <Card.Image src={pokemon.img} width="100%" height={800} />
+              </Card.Body>
+              <Card.Footer>
+                <Row justify="space-between">
+                  <Text transform="capitalize">{pokemon.name}</Text>
+                  <Text># {pokemon.id}</Text>
+                </Row>
+              </Card.Footer>
+            </Card>
+          </Grid>
+        ))}
+      </Grid.Container>
     </Layout>
   );
 };
