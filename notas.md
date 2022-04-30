456a0003c4777406dce42b5cbbc4aed242f91e44
Daniel Unapanta <dunapanta@outlook.es>
2022-04-29 22:28:34 -0500

    Add getStaticPaths

--- notas.md
+++ notas.md
@@ -1,5 +1,7 @@
-## Clase 45 GetStaticProps
+## Clase 45 getStaticProps
 
 - getStaticProps Es una función que se ejecuta unicamente en el lado del servidory solo se ejecuta en build time
 - Solo se puede ejecutar en pages
 
+# Clase 54 getStaticPaths
+- Si la página tiene rutas dinamicas y usa `getStaticProps`, se necesita definir una lista de paths para ser generados estaticamente
--- pages/pokemon/[pokemonId].tsx
+++ pages/pokemon/[pokemonId].tsx
@@ -1,11 +1,48 @@
+import { GetStaticPaths, GetStaticProps, NextPage } from "next";
+import { useRouter } from "next/router";
+
 import { Layout } from "components/layouts";
 
-const SinglePokemon = () => {
+interface Props {
+  id: string;
+  name: string;
+}
+
+const SinglePokemon: NextPage<Props> = ({ id, name }: Props) => {
+  const router = useRouter();
   return (
     <Layout title="">
-      <h1>Pokemon</h1>
+      <h1>
+        {id} - {name}
+      </h1>
     </Layout>
   );
+};
+
+export const getStaticPaths: GetStaticPaths = async (ctx) => {
+  return {
+    paths: [
+      {
+        params: {
+          pokemonId: "1",
+        },
+      },
+    ],
+    fallback: false,
+  };
+};
+
+export const getStaticProps: GetStaticProps = async (context) => {
+  /* const { data: pokemons } = await pokemonApi.get<PokemonListResponse>(
+    "/pokemon?limit=151"
+  ); */
+
+  return {
+    props: {
+      id: 1,
+      name: "Bulbasor",
+    },
+  };
 };
 
 export default SinglePokemon;
