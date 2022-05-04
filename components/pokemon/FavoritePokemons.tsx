d7a4594709e6946265b2416a743b2f63c0705674
Daniel Unapanta <dunapanta@outlook.es>
2022-05-03 20:11:22 -0500

    Add routing to favorite pokemon

--- components/pokemon/FavoritePokemons.tsx
+++ components/pokemon/FavoritePokemons.tsx
@@ -1,16 +1,26 @@
 import React from "react";
 import { Card, Grid } from "@nextui-org/react";
+import { useRouter } from "next/router";
 
 interface Props {
   favoritesPokemon: number[];
 }
 
 export const FavoritePokemons = ({ favoritesPokemon }: Props) => {
+  const router = useRouter();
+  const onFavoritePokemonClick = (id: number) => {
+    router.push(`/pokemon/${id}`);
+  };
   return (
     <Grid.Container justify="flex-start" direction="row" gap={2}>
       {favoritesPokemon.map((id) => (
         <Grid xs={6} sm={3} md={2} xl={1} key={id}>
-          <Card hoverable clickable css={{ padding: "10px" }}>
+          <Card
+            hoverable
+            clickable
+            onClick={() => onFavoritePokemonClick(id)}
+            css={{ padding: "10px" }}
+          >
             <Card.Image
               src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${id}.svg`}
               width="100%"
